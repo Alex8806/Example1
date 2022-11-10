69519be0589d9ef0195dd3557957cb7fd7bb9f95
Alex <alex@Mail.com>
2022-11-10 12:24:20 +0200

    change in sum and diff

--- Example04_dif/Program.cs
+++ Example04_dif/Program.cs
@@ -1,4 +1,6 @@
-﻿double NumberA =324536453;
-double NumberB=5;
+﻿double NumberA =new Random().Next(1,191);
+Console.WriteLine(NumberA);
+double NumberB=new Random().Next(1,191);
+Console.WriteLine(NumberB);
 Console.WriteLine(NumberA/NumberB);
 
