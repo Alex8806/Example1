c1f7d92190fb63acd20e4cf3f7aedea3eb599695
Alex <alex@Mail.com>
2022-11-10 12:37:10 +0200

    Masha user

---
+++ Example05_ifelseUser/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Insert user name");
+string userName = Console.ReadLine();
+ if(userName.ToLower == "masha")
+ {
+    Console.WriteLine ("Ura, Masha is here");
+ }
+else
+{Console.Write("Hello usual user,");
+Console.Write(userName);}
